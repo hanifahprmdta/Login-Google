1545c7b94abb9f0c9b823332011d3657fdca140c
hnifhprmdta678@gmail.com <hnifhprmdta678@gmail.com>
2023-02-03 15:23:56 +0700

    login google

---
+++ src/config.jsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbrWmW0Id6_7unJ59g6dRIef3pfJYcVb8",
+  authDomain: "login-hanifah.firebaseapp.com",
+  projectId: "login-hanifah",
+  storageBucket: "login-hanifah.appspot.com",
+  messagingSenderId: "885656287167",
+  appId: "1:885656287167:web:0d91fca3da2f8f4b8f930a",
+  measurementId: "G-660FE3BW0Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export default auth
